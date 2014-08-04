01966d10a8fc9a687997121586c3589daf772a68
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-08-04 14:27:18 +0000

    SeasonNamer: included series' name into the name

---  subcentral-core/subcentral-core/src/main/java/de/subcentral/core/naming/SeasonNamer.java
+++  subcentral-core/subcentral-core/src/main/java/de/subcentral/core/naming/SeasonNamer.java
@@ -3,6 +3,7 @@ package de.subcentral.core.naming;
 import java.util.Map;
 
 import de.subcentral.core.model.media.Season;
+import de.subcentral.core.model.media.Series;
 
 public class SeasonNamer extends AbstractPropertySequenceNamer<Season>
 {
@@ -16,6 +17,10 @@ public class SeasonNamer extends AbstractPropertySequenceNamer<Season>
 	protected String doName(Season season, NamingService namingService, Map<String, Object> parameters) throws Exception
 	{
 		Builder b = new Builder();
+		if (season.getSeries() != null)
+		{
+			b.append(Series.PROP_NAME, season.getSeries());
+		}
 		b.appendIf(Season.PROP_NUMBER, season.getNumber(), season.isNumbered());
 		b.appendIf(Season.PROP_TITLE, season.getTitle(), season.isTitled());
 		return b.build();
