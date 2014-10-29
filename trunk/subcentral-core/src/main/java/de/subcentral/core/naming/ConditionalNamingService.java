91918771aa5398eba1dc4bcbf058cbbd1590fc62
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-10-29 14:41:43 +0000

    Rearranged the namers in the default naming service.

--- trunk/subcentral-core/src/main/java/de/subcentral/core/naming/ConditionalNamingService.java
+++ trunk/subcentral-core/src/main/java/de/subcentral/core/naming/ConditionalNamingService.java
@@ -32,7 +32,7 @@ public class ConditionalNamingService implements NamingService
 	 * Important:
 	 * <ul>
 	 * <li>The order of the elements is the order the conditions are tested. So more restricting conditions must be placed before more general
-	 * conditions.</li>
+	 * conditions. The first ConditionalNamer which condition returns true will be taken.</li>
 	 * <li>The order should also consider how often specific types are named. The types that are named most frequently should be at the top of the
 	 * list.</li>
 	 * </ul>
