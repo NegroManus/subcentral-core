f60b5a1192862dd751be2434a2afe157b9562522
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-11-05 13:36:22 +0000

    Improved Standardizings.

--- trunk/subcentral-core/src/test/java/de/subcentral/core/standardizing/StandardizingTest.java
+++ trunk/subcentral-core/src/test/java/de/subcentral/core/standardizing/StandardizingTest.java
@@ -1,13 +1,20 @@
 package de.subcentral.core.standardizing;
 
 import java.util.List;
+import java.util.regex.Pattern;
 
 import org.junit.Test;
 
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 
 import de.subcentral.core.model.media.Episode;
+import de.subcentral.core.model.media.Season;
+import de.subcentral.core.model.media.Series;
 import de.subcentral.core.model.release.Release;
+import de.subcentral.core.model.subtitle.Subtitle;
+import de.subcentral.core.model.subtitle.SubtitleAdjustment;
+import de.subcentral.core.util.PatternReplacer;
 
 public class StandardizingTest
 {
@@ -25,11 +32,24 @@ public class StandardizingTest
 	@Test
 	public void testReflectiveStandardizing()
 	{
-		ReflectiveStandardizer<Episode> stdzer = new ReflectiveStandardizer<>(Episode.class, ImmutableMap.of("numberInSeason", (Integer n) -> n + 1));
+		ClassBasedStandardizingService service = new ClassBasedStandardizingService("test");
+		service.registerNestedBeanRetriever(Episode.class, Standardizings::retrieveNestedBeans);
+		service.registerNestedBeanRetriever(Season.class, Standardizings::retrieveNestedBeans);
+		service.registerNestedBeanRetriever(Release.class, Standardizings::retrieveNestedBeans);
+		service.registerNestedBeanRetriever(Subtitle.class, Standardizings::retrieveNestedBeans);
+		service.registerNestedBeanRetriever(SubtitleAdjustment.class, Standardizings::retrieveNestedBeans);
+		service.registerStandardizer(Episode.class, e -> {
+			Boolean oldVal = Boolean.valueOf(e.isSpecial());
+			e.setSpecial(true);
+			return ImmutableList.of(new StandardizingChange(e, Episode.PROP_SPECIAL, oldVal, Boolean.TRUE));
+		});
+		service.registerStandardizer(Series.class,
+				new SeriesNamerStandardizer(new PatternReplacer(ImmutableMap.<Pattern, String> of(Pattern.compile("Psych"), "Psych (2001)"))));
 
 		Episode epi = Episode.createSeasonedEpisode("Psych", 2, 2);
 
-		List<StandardizingChange> changes = stdzer.standardize(epi);
+		List<StandardizingChange> changes = service.standardize(epi);
 		changes.stream().forEach(c -> System.out.println(c));
+		System.out.println(epi);
 	}
 }
