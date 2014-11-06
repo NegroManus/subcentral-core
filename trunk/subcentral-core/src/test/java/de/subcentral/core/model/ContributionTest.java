9682827b781e6ad4b1f73ae5e5b0c682f1b8644a
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-11-06 13:07:19 +0000

    Contribution.contributor now non-nullable.

--- trunk/subcentral-core/src/test/java/de/subcentral/core/model/ContributionTest.java
+++ trunk/subcentral-core/src/test/java/de/subcentral/core/model/ContributionTest.java
@@ -12,19 +12,19 @@ public class ContributionTest
 	public static void main(String[] args)
 	{
 		List<Contribution> clist = new ArrayList<>();
-		Contribution c1 = new Contribution();
+		Contribution c1 = new Contribution(new SimpleContributor());
 		c1.setType(Subtitle.CONTRIBUTION_TYPE_TRANSLATION);
 		c1.setAmount(50);
 		c1.setProgress(1.0d);
-		Contribution c2 = new Contribution();
+		Contribution c2 = new Contribution(new SimpleContributor());
 		c2.setType(Subtitle.CONTRIBUTION_TYPE_TRANSLATION);
 		c2.setAmount(25);
 		c2.setProgress(0.0d);
-		Contribution c3 = new Contribution();
+		Contribution c3 = new Contribution(new SimpleContributor());
 		c3.setType(Subtitle.CONTRIBUTION_TYPE_TRANSLATION);
 		c3.setAmount(25);
 		c3.setProgress(0.5d);
-		Contribution cb1 = new Contribution();
+		Contribution cb1 = new Contribution(new SimpleContributor());
 		cb1.setType(Subtitle.CONTRIBUTION_TYPE_REVISION);
 		cb1.setAmount(1L);
 		cb1.setProgress(1d);
@@ -42,6 +42,14 @@ public class ContributionTest
 			System.out.println(duration + "ms");
 			System.out.println(progresses);
 		}
+	}
 
+	private static class SimpleContributor implements Contributor
+	{
+		@Override
+		public String getName()
+		{
+			return "Simple Contributor";
+		}
 	}
 }
