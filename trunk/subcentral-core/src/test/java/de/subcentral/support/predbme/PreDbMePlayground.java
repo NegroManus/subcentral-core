b22edadb0f98aaaf0a49c51ff146131ce18ad8b3
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-11-13 14:04:21 +0000

    Media.furtherInfoLinks, coverLinks

---
+++ trunk/subcentral-core/src/test/java/de/subcentral/support/predbme/PreDbMePlayground.java
@@ -0,0 +1,27 @@
+package de.subcentral.support.predbme;
+
+import java.io.File;
+import java.net.URL;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import com.google.common.io.Resources;
+
+import de.subcentral.core.model.release.Release;
+
+public class PreDbMePlayground
+{
+
+	public static void main(String[] args) throws Exception
+	{
+		PreDbMeLookup lookup = new PreDbMeLookup();
+
+		URL url = Resources.getResource("de/subcentral/core/impl/predb/icarly.s01e10_details_formatted.html");
+		File resource = new File(url.toURI());
+		Document doc = Jsoup.parse(resource, "UTF-8", lookup.getHost().toExternalForm());
+
+		Release rls = lookup.parseReleaseDetails(doc, new Release());
+		System.out.println(rls);
+	}
+}
