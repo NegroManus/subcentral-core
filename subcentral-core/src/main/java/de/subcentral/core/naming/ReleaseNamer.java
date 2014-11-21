453968be9de017de0c5ed1e12c5f81039da5a050
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-11-21 10:07:26 +0000

    ReleaseNamer, SubtitleAdjustmentNamer: Renamed naming parameter "useName" to "preferName" and changed its meaning: If preferName == true && name != null, then return the name. Otherwise generate the name.

--- subcentral-core/src/main/java/de/subcentral/core/naming/ReleaseNamer.java
+++ subcentral-core/src/main/java/de/subcentral/core/naming/ReleaseNamer.java
@@ -11,9 +11,10 @@ import de.subcentral.core.util.Separation;
 public class ReleaseNamer extends AbstractPropertySequenceNamer<Release>
 {
 	/**
-	 * The parameter key for the Boolean value "useName". The default value is {@code false}.
+	 * The parameter key for the Boolean value "preferName". The default value is {@code false}. If set to true, the release's name (if not null) is
+	 * returned as the name.
 	 */
-	public static final String	PARAM_USE_NAME_KEY	= "useName";
+	public static final String	PARAM_PREFER_NAME_KEY	= "preferName";
 
 	private final NamingService	mediaNamingService;
 
@@ -33,11 +34,11 @@ public class ReleaseNamer extends AbstractPropertySequenceNamer<Release>
 	public void buildName(PropSequenceNameBuilder b, Release rls, Map<String, Object> params)
 	{
 		// read naming parameters
-		boolean useName = Namings.readParameter(params, PARAM_USE_NAME_KEY, Boolean.class, Boolean.FALSE);
+		boolean preferName = Namings.readParameter(params, PARAM_PREFER_NAME_KEY, Boolean.class, Boolean.FALSE);
 
-		if (useName)
+		if (preferName && rls.getName() != null)
 		{
-			b.appendIfNotNull(Release.PROP_NAME, rls.getName());
+			b.append(Release.PROP_NAME, rls.getName());
 			return;
 		}
 		b.appendIfNotBlank(Release.PROP_MEDIA, mediaNamingService.name(rls.getMedia(), params));
