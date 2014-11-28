fc0925a546fe74eb63334ce7b04874f56b4e450f
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2014-11-28 13:30:22 +0000

    Javadoc minor;

--- subcentral-core/src/main/java/de/subcentral/core/naming/ReleaseNamer.java
+++ subcentral-core/src/main/java/de/subcentral/core/naming/ReleaseNamer.java
@@ -12,7 +12,7 @@ public class ReleaseNamer extends AbstractPropertySequenceNamer<Release>
 {
 	/**
 	 * The name of the parameter "preferName" of type {@link Boolean}. If set to {@code true} and the {@link Release#getName() release's name} is not
-	 * {@code null}, this name is returned as the name, otherwise the computed name is returned. The default value is {@code false}.
+	 * {@code null}, that name is returned, otherwise the computed name is returned. The default value is {@code false}.
 	 */
 	public static final String	PARAM_PREFER_NAME	= ReleaseNamer.class.getName() + ".preferName";
 
