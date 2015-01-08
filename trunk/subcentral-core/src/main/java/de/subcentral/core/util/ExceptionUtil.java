eb581ab4aa71d0058ea843d735104704d251191d
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2015-01-08 13:54:08 +0000

    Core: added ExceptionUtil

---
+++ trunk/subcentral-core/src/main/java/de/subcentral/core/util/ExceptionUtil.java
@@ -0,0 +1,19 @@
+package de.subcentral.core.util;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+
+public class ExceptionUtil
+{
+	public static String stackTraceToString(Throwable t)
+	{
+		StringWriter sw = new StringWriter();
+		t.printStackTrace(new PrintWriter(sw));
+		return sw.toString();
+	}
+
+	private ExceptionUtil()
+	{
+		throw new AssertionError(getClass() + " is an utility class and therefore cannot be instantiated");
+	}
+}
