d231bf20488534a39b84a7de0d24418259fe3f6c
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2015-01-13 10:56:55 +0000

    Reworked SeriesNameStandardizer to SeriesNameAndTitleStandardizer;

---
+++ subcentral-core/src/main/java/de/subcentral/core/standardizing/SeriesNameAndTitleStandardizer.java
@@ -0,0 +1,65 @@
+package de.subcentral.core.standardizing;
+
+import java.util.List;
+import java.util.Objects;
+import java.util.regex.Pattern;
+
+import com.google.common.collect.ImmutableList;
+
+import de.subcentral.core.model.media.Series;
+
+/**
+ * @implSpec #immutable #thread-safe
+ */
+public class SeriesNameAndTitleStandardizer implements Standardizer<Series>
+{
+	private final Pattern	namePattern;
+	private final String	nameReplacement;
+	private final String	titleReplacement;
+
+	public SeriesNameAndTitleStandardizer(Pattern namePattern, String nameReplacement, String titleReplacement)
+	{
+		this.namePattern = Objects.requireNonNull(namePattern, "namePattern");
+		this.nameReplacement = Objects.requireNonNull(nameReplacement, "nameReplacement");
+		this.titleReplacement = Objects.requireNonNull(titleReplacement, "titleReplacement");
+	}
+
+	public Pattern getNamePattern()
+	{
+		return namePattern;
+	}
+
+	public String getNameReplacement()
+	{
+		return nameReplacement;
+	}
+
+	public String getTitleReplacement()
+	{
+		return titleReplacement;
+	}
+
+	@Override
+	public List<StandardizingChange> standardize(Series series)
+	{
+		String oldName = series.getName();
+		if (oldName != null && namePattern.matcher(oldName).matches())
+		{
+			ImmutableList.Builder<StandardizingChange> changes = ImmutableList.builder();
+			if (!oldName.equals(nameReplacement))
+			{
+				series.setName(nameReplacement);
+				changes.add(new StandardizingChange(series, Series.PROP_NAME.getPropName(), oldName, nameReplacement));
+			}
+
+			String oldTitle = series.getTitle();
+			if (!Objects.equals(oldTitle, titleReplacement))
+			{
+				series.setTitle(titleReplacement);
+				changes.add(new StandardizingChange(series, Series.PROP_TITLE.getPropName(), oldTitle, titleReplacement));
+			}
+			return changes.build();
+		}
+		return ImmutableList.of();
+	}
+}
