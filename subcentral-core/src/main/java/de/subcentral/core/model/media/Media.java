8895c3f74cc85b327b87faabd99009e62187b923
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2015-01-13 10:20:56 +0000

    Media minor: javadoc;

--- subcentral-core/src/main/java/de/subcentral/core/model/media/Media.java
+++ subcentral-core/src/main/java/de/subcentral/core/model/media/Media.java
@@ -90,8 +90,15 @@ public interface Media extends Work
 
 	// Property accessors
 	/**
-	 * Normally a media only has a {@link NamedMedia#getName() name} which is in fact its title. The title is only set if it differs from the name
-	 * (like name: "The Office (UK)", title: "The Office").<br>
+	 * the unique name of this media
+	 * 
+	 * @return
+	 */
+	public String getName();
+
+	/**
+	 * Normally a media only has a {@link #getName() name} which is in fact its title. The title is only set if it differs from the name (like name:
+	 * "The Office (UK)", title: "The Office").<br>
 	 * In case of numbered Media, like an {@link Episode} or a Song, the title may be optional.
 	 * 
 	 * @return the title of this media
@@ -172,6 +179,24 @@ public interface Media extends Work
 	public ListMultimap<String, Object> getAttributes();
 
 	// Convenience
+	/**
+	 * For NamedMedia the title is the name without the extension to identify the media (e.g. without the year "(2014)" or country code "(UK)"). If
+	 * the title equals the name, no title is specified.
+	 * <p>
+	 * Examples
+	 * <ul>
+	 * <li>name: "The Office (UK)" -> title: "The Office"</li>
+	 * <li>name: "Titanic (2002)" -> title: "Titanic".</li>
+	 * </ul>
+	 * </p>
+	 * 
+	 * @return the {@link #getTitle() title} if this media {@link #isTitled() is titled}, else the {@link #getName() name}
+	 */
+	public default String getTitleOrName()
+	{
+		return isTitled() ? getTitle() : getName();
+	}
+
 	/**
 	 * 
 	 * @return {@code true} if this media has a title, {@code false} otherwise
