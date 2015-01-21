df728875aca27b12f309a7440c852df738f0549c
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2015-01-21 10:45:31 +0000

    Series: removed episode and season lists;

--- subcentral-core/src/main/java/de/subcentral/core/model/media/MediaCollection.java
+++
@@ -1,8 +0,0 @@
-package de.subcentral.core.model.media;
-
-import java.util.List;
-
-public interface MediaCollection<M extends Media> extends Media
-{
-	public List<M> getMediaItems();
-}
--- subcentral-core/src/main/java/de/subcentral/core/model/media/Series.java
+++ subcentral-core/src/main/java/de/subcentral/core/model/media/Series.java
@@ -2,28 +2,22 @@ package de.subcentral.core.model.media;
 
 import java.time.temporal.Temporal;
 import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
-import java.util.Map;
-import java.util.Objects;
 import java.util.Set;
 
 import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 
 import com.google.common.base.MoreObjects;
-import com.google.common.collect.ImmutableList;
 
 import de.subcentral.core.Settings;
 import de.subcentral.core.model.ModelUtils;
 import de.subcentral.core.model.PropNames;
 import de.subcentral.core.util.SimplePropDescriptor;
 
-public class Series extends AbstractNamedMedia implements AvMediaCollection<Episode>, Comparable<Series>
+public class Series extends AbstractNamedMedia implements Comparable<Series>
 {
 	public static final SimplePropDescriptor	PROP_NAME					= new SimplePropDescriptor(Series.class, PropNames.NAME);
 	public static final SimplePropDescriptor	PROP_TITLE					= new SimplePropDescriptor(Series.class, PropNames.TITLE);
@@ -78,9 +72,6 @@ public class Series extends AbstractNamedMedia implements AvMediaCollection<Epis
 	private int									regularRunningTime			= 0;
 	// HashMap / HashSet initial capacities should be a power of 2
 	private final Set<String>					genres						= new HashSet<>(4);
-	// Normally, the Seasons and Episodes are not stored in the Series
-	private final List<Season>					seasons						= new ArrayList<>(0);
-	private final List<Episode>					episodes					= new ArrayList<>(0);
 
 	public Series()
 	{}
@@ -217,44 +208,6 @@ public class Series extends AbstractNamedMedia implements AvMediaCollection<Epis
 		return countriesOfOrigin.isEmpty() ? null : countriesOfOrigin.get(0);
 	}
 
-	/**
-	 * @return the air date ({@link Episode#getDate()}) of the first episode of this series or <code>null</code> if this series has no episodes
-	 */
-	public Temporal getDateOfFirstEpisode()
-	{
-		return episodes.isEmpty() ? null : episodes.get(0).getDate();
-	}
-
-	/**
-	 * @return the air date ({@link Episode#getDate()}) of the last episode of this series or <code>null</code> if this series has no episodes
-	 */
-	public Temporal getDateOfLastEpisode()
-	{
-		return episodes.isEmpty() ? null : episodes.get(episodes.size() - 1).getDate();
-	}
-
-	// Episodes
-	@Override
-	public List<Episode> getMediaItems()
-	{
-		return getEpisodes();
-	}
-
-	/**
-	 * 
-	 * @return This series' episodes. Only filled if the series is the information root.
-	 */
-	public List<Episode> getEpisodes()
-	{
-		return episodes;
-	}
-
-	public void setEpisodes(Collection<Episode> episodes)
-	{
-		this.episodes.clear();
-		this.episodes.addAll(episodes);
-	}
-
 	// == newEpisode() methods ==
 	// Generic
 	public Episode newEpisode()
@@ -310,93 +263,7 @@ public class Series extends AbstractNamedMedia implements AvMediaCollection<Epis
 		return new Episode(this, date, title);
 	}
 
-	// == addEpisode() methods ==
-	// Generic
-	public Episode addEpisode()
-	{
-		Episode epi = new Episode(this);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(String title)
-	{
-		Episode epi = new Episode(this, title);
-		episodes.add(epi);
-		return epi;
-	}
-
-	// Seasoned
-	public Episode addEpisode(Season season)
-	{
-		Episode epi = new Episode(this, season);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(Season season, Integer numberInSeason)
-	{
-		Episode epi = new Episode(this, season, numberInSeason);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(Season season, String title)
-	{
-		Episode epi = new Episode(this, season, title);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(Season season, Integer numberInSeason, String title)
-	{
-		Episode epi = new Episode(this, season, numberInSeason, title);
-		episodes.add(epi);
-		return epi;
-	}
-
-	// Mini-series
-	public Episode addEpisode(Integer numberInSeries)
-	{
-		Episode epi = new Episode(this, numberInSeries);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(Integer numberInSeries, String title)
-	{
-		Episode epi = new Episode(this, numberInSeries, title);
-		episodes.add(epi);
-		return epi;
-	}
-
-	// Dated
-	public Episode addEpisode(Temporal date)
-	{
-		Episode epi = new Episode(this, date);
-		episodes.add(epi);
-		return epi;
-	}
-
-	public Episode addEpisode(Temporal date, String title)
-	{
-		Episode epi = new Episode(this, date, title);
-		episodes.add(epi);
-		return epi;
-	}
-
 	// Seasons
-	public List<Season> getSeasons()
-	{
-		return seasons;
-	}
-
-	public void setSeasons(Collection<Season> seasons)
-	{
-		this.seasons.clear();
-		this.seasons.addAll(seasons);
-	}
-
 	public Season newSeason()
 	{
 		return new Season(this);
@@ -410,79 +277,6 @@ public class Series extends AbstractNamedMedia implements AvMediaCollection<Epis
 	public Season newSeason(String title)
 	{
 		return new Season(this, title);
-	}
-
-	public Season addSeason()
-	{
-		Season season = new Season(this);
-		seasons.add(season);
-		return season;
-	}
-
-	public Season addSeason(Integer number)
-	{
-		Season season = new Season(this, number);
-		seasons.add(season);
-		return season;
-	}
-
-	public Season addSeason(String title)
-	{
-		Season season = new Season(this, title);
-		seasons.add(season);
-		return season;
-	}
-
-	// Seasons and Episode
-	public List<Episode> getEpisodesOf(Season season)
-	{
-		if (episodes.isEmpty())
-		{
-			return ImmutableList.of();
-		}
-		ImmutableList.Builder<Episode> episInSeason = ImmutableList.builder();
-		for (Episode epi : episodes)
-		{
-			if (Objects.equals(season, epi.getSeason()))
-			{
-				episInSeason.add(epi);
-			}
-		}
-		return episInSeason.build();
-	}
-
-	/**
-	 * Returns an immutable map of all the seasons of this series mapped to their corresponding episodes, plus {@code null} mapped to the episodes
-	 * which are not part of a season.<br/>
-	 * The map contains a {@code null} key which value is a list of all the episodes with no season, this list may be empty but it is never
-	 * {@code null}. The map may contain a mapping between a season and an empty list for seasons without episodes.<br/>
-	 * The seasons (the keys) are in the same order as in the {@link #getSeasons() seasons list}. The episodes for each season are in the same order
-	 * as in the {@link #getEpisodes() episodes list}.
-	 * 
-	 * @return an immutable map with the seasons as keys (including the {@code null} season) and their episodes as values
-	 * @throws IllegalStateException
-	 *             if the season of an episode in the {@link #getEpisodes() episodes list} is not contained in the {@link #getSeasons() seasons list}
-	 */
-	public Map<Season, List<Episode>> getSeasonsToEpisodesMap() throws IllegalStateException
-	{
-		// preserve insertion order -> use a LinkedHashMap
-		Map<Season, List<Episode>> seasonsAndEpis = new LinkedHashMap<>(seasons.size());
-		for (Season s : seasons)
-		{
-			seasonsAndEpis.put(s, new ArrayList<>());
-		}
-		// for the Episodes without a Season
-		seasonsAndEpis.put(null, new ArrayList<>());
-		for (Episode epi : episodes)
-		{
-			List<Episode> seasonEpiList = seasonsAndEpis.get(epi.getSeason());
-			if (seasonEpiList == null)
-			{
-				throw new IllegalStateException("The seasons list of this series does not contain the season of the following episode: " + epi);
-			}
-			seasonEpiList.add(epi);
-		}
-		return Collections.unmodifiableMap(seasonsAndEpis);
 	}
 
 	// Object methods
@@ -538,8 +332,6 @@ public class Series extends AbstractNamedMedia implements AvMediaCollection<Epis
 				.add("contributions", ModelUtils.nullIfEmpty(contributions))
 				.add("furtherInfoLinks", ModelUtils.nullIfEmpty(furtherInfoLinks))
 				.add("attributes", ModelUtils.nullIfEmpty(attributes))
-				.add("episodes.size", ModelUtils.nullIfZero(episodes.size()))
-				.add("seasons.size", ModelUtils.nullIfZero(seasons.size()))
 				.toString();
 	}
 }
