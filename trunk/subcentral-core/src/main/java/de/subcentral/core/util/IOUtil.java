87153d01bc69596a64ca971900c354d7b4df6c96
negromanus90@gmail.com <negromanus90@gmail.com@45933e35-b4a2-7a13-25ed-74bb039d1447>
2015-03-02 16:00:18 +0000

    subcentral-core, watcher: If file is blocked, the FileProcessingTask waits until it is written completely and the lock is released before trying to copy that file. Thus, avoiding NoSuchFileExeptions;

--- trunk/subcentral-core/src/main/java/de/subcentral/core/util/IOUtil.java
+++ trunk/subcentral-core/src/main/java/de/subcentral/core/util/IOUtil.java
@@ -1,8 +1,13 @@
 package de.subcentral.core.util;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.RandomAccessFile;
 import java.nio.charset.Charset;
+import java.nio.file.Path;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import org.apache.commons.lang3.ArrayUtils;
 
@@ -33,6 +38,58 @@ public class IOUtil
 			return new String[] { filename, "" };
 		}
 		return new String[] { filename.substring(0, indexOfLastDot), filename.substring(indexOfLastDot, filename.length()) };
+	}
+
+	public static boolean isCompletelyWritten(Path file)
+	{
+		RandomAccessFile stream = null;
+		try
+		{
+			stream = new RandomAccessFile(file.toFile(), "rw");
+			return true;
+		}
+		catch (Exception e)
+		{
+			// ignore
+			e.printStackTrace();
+		}
+		finally
+		{
+			if (stream != null)
+			{
+				try
+				{
+					stream.close();
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+				}
+			}
+		}
+		return false;
+	}
+
+	public static boolean waitUntilCompletelyWritten(Path file, long timeout, TimeUnit timeoutUnit) throws TimeoutException
+	{
+		long start = System.currentTimeMillis();
+		while (!isCompletelyWritten(file))
+		{
+			if (System.currentTimeMillis() >= start + timeoutUnit.toMillis(timeout))
+			{
+				throw new TimeoutException("Timed out after " + timeout + " " + timeoutUnit + " while waiting for file " + file
+						+ " to become completely written (unlocked)");
+			}
+			try
+			{
+				Thread.sleep(50);
+			}
+			catch (InterruptedException e)
+			{
+				e.printStackTrace();
+			}
+		}
+		return true;
 	}
 
 	private IOUtil()
