32a492a75119aabd305ee16d0b22a3341b0a44fe
NegroManus <negromanus@yahoo.de>
2015-09-17 20:39:45 +0200

    updated app_version_date;

--- subcentral-watcher/src/main/java/de/subcentral/watcher/WatcherApp.java
+++ subcentral-watcher/src/main/java/de/subcentral/watcher/WatcherApp.java
@@ -19,7 +19,7 @@ public class WatcherApp extends Application
 {
 	public static final String	APP_NAME			= "Watcher";
 	public static final String	APP_VERSION			= "2.0";
-	public static final String	APP_VERSION_DATE	= "2015-09-10 18:30";
+	public static final String	APP_VERSION_DATE	= "2015-09-17 21:00";
 	public static final String	APP_INFO			= APP_NAME + " " + APP_VERSION + " (" + APP_VERSION_DATE + ")";
 
 	private static final Logger log = LogManager.getLogger(WatcherApp.class);
