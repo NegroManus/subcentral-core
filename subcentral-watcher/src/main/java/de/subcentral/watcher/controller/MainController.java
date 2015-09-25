9b6f59e9359190d2bbdb9ba7e905316ab766327a
negromanus <negromanus90@gmail.com>
2015-09-25 18:08:42 +0200

    Improved the system tray handling;
    Added exit and hide img;

--- subcentral-watcher/src/main/java/de/subcentral/watcher/controller/MainController.java
+++ subcentral-watcher/src/main/java/de/subcentral/watcher/controller/MainController.java
@@ -1,6 +1,5 @@
 package de.subcentral.watcher.controller;
 
-import java.awt.AWTException;
 import java.awt.MenuItem;
 import java.awt.PopupMenu;
 import java.awt.SystemTray;
@@ -17,6 +16,8 @@ import java.util.concurrent.SynchronousQueue;
 import java.util.concurrent.ThreadFactory;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
+
+import javax.swing.SwingUtilities;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -57,7 +58,8 @@ public class MainController extends AbstractController
 
 	// SystemTray handling
 	private SystemTray	systemTray;
-	private TrayIcon	trayIcon;
+	private TrayIcon	systemTrayIcon;
+	private MenuItem	systemTrayShowHideMenuItem;
 
 	// Controller
 	private WatchController			watchController;
@@ -82,43 +84,6 @@ public class MainController extends AbstractController
 
 		// System Tray
 		initSystemTrayIcon();
-	}
-
-	public Stage getPrimaryStage()
-	{
-		return primaryStage;
-	}
-
-	public ExecutorService getCommonExecutor()
-	{
-		return commonExecutor;
-	}
-
-	public WinRar getWinRar()
-	{
-		WinRar winRar = WinRar.getInstance();
-		winRar.setProcessExecutor(commonExecutor);
-		return winRar;
-	}
-
-	public WatchController getWatchController()
-	{
-		return watchController;
-	}
-
-	public ProcessingController getProcessingController()
-	{
-		return processingController;
-	}
-
-	public SettingsController getSettingsController()
-	{
-		return settingsController;
-	}
-
-	public void selectTab(int index)
-	{
-		tabPane.getSelectionModel().select(index);
 	}
 
 	private void initCommonExecutor()
@@ -155,17 +120,7 @@ public class MainController extends AbstractController
 	private void initProcessingController() throws IOException
 	{
 		processingController = new ProcessingController(this);
-		loadProcessingPane();
-	}
-
-	public void loadProcessingPane() throws IOException
-	{
-		BorderPane processingPane = FxUtil.loadFromFxml("ProcessingView.fxml", "ProcessingView", Locale.ENGLISH, processingController);
-		AnchorPane.setTopAnchor(processingPane, 0.0d);
-		AnchorPane.setRightAnchor(processingPane, 0.0d);
-		AnchorPane.setBottomAnchor(processingPane, 0.0d);
-		AnchorPane.setLeftAnchor(processingPane, 0.0d);
-		processingRootPane.getChildren().setAll(processingPane);
+		reloadProcessingPane();
 	}
 
 	private void initWatchController() throws IOException
@@ -177,77 +132,161 @@ public class MainController extends AbstractController
 
 	private void initSystemTrayIcon()
 	{
-		javax.swing.SwingUtilities.invokeLater(() ->
+		SwingUtilities.invokeLater(() ->
 		{
 			// Check the SystemTray is supported
 			if (!SystemTray.isSupported())
 			{
-				log.warn("SystemTray is not supported");
+				log.warn("System tray is not supported");
 				return;
 			}
-
 			try
 			{
-				// Convention for tray icons: set the default icon for opening the application stage in a bold font
-				java.awt.Font defaultFont = java.awt.Font.decode(null);
-				java.awt.Font boldFont = defaultFont.deriveFont(java.awt.Font.BOLD);
-
 				systemTray = SystemTray.getSystemTray();
+				ActionListener showHideListener = (ActionEvent e) -> Platform.runLater(this::toggleShowHide);
+
 				java.awt.Image trayImg = FxUtil.loadAwtImg("watcher_16.png");
-				trayIcon = new TrayIcon(trayImg);
+				systemTrayIcon = new TrayIcon(trayImg);
+				// Show/Hide on double-click
+				systemTrayIcon.addActionListener(showHideListener);
 
 				PopupMenu popup = new PopupMenu();
-				MenuItem showHideItem = new MenuItem("Hide");
-				ActionListener showHideListener = (ActionEvent e) -> Platform.runLater(() ->
-				{
-					if (primaryStage.isShowing())
-					{
-						primaryStage.hide();
-						showHideItem.setLabel("Show");
-						showHideItem.setFont(boldFont);
-					}
-					else
-					{
-						primaryStage.show();
-						showHideItem.setLabel("Hide");
-						showHideItem.setFont(defaultFont);
-					}
-				});
-				showHideItem.addActionListener(showHideListener);
+
+				MenuItem appItem = new MenuItem("Watcher");
+				appItem.setFont(java.awt.Font.decode(null).deriveFont(java.awt.Font.BOLD));
+
+				systemTrayShowHideMenuItem = new MenuItem("Hide");
+				systemTrayShowHideMenuItem.addActionListener(showHideListener);
 
 				MenuItem exitItem = new MenuItem("Exit");
-				exitItem.addActionListener((ActionEvent e) -> Platform.runLater(() -> primaryStage.fireEvent(new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST))));
+				exitItem.addActionListener((ActionEvent e) -> Platform.runLater(this::exit));
 
 				// Add components to pop-up menu
-				popup.add(showHideItem);
+				popup.add(appItem);
+				popup.addSeparator();
+				popup.add(systemTrayShowHideMenuItem);
 				popup.addSeparator();
 				popup.add(exitItem);
 
-				trayIcon.setPopupMenu(popup);
-				// Show/Hide on double-click
-				trayIcon.addActionListener(showHideListener);
+				systemTrayIcon.setPopupMenu(popup);
 
-				systemTray.add(trayIcon);
-				log.info("Added SystemTray icon");
+				systemTray.add(systemTrayIcon);
 			}
-			catch (IOException | AWTException e)
+			catch (Exception e)
 			{
-				log.warn("SystemTray icon could not be added", e);
+				log.warn("System tray icon could not be added", e);
+
+				// Clean up just in case. So that no uninitialized SystemTrayIcon is displayed
+				removeSystemTrayIcon();
 			}
 		});
 	}
 
+	// Public API
+	public Stage getPrimaryStage()
+	{
+		return primaryStage;
+	}
+
+	public ExecutorService getCommonExecutor()
+	{
+		return commonExecutor;
+	}
+
+	public WinRar getWinRar()
+	{
+		WinRar winRar = WinRar.getInstance();
+		winRar.setProcessExecutor(commonExecutor);
+		return winRar;
+	}
+
+	public WatchController getWatchController()
+	{
+		return watchController;
+	}
+
+	public ProcessingController getProcessingController()
+	{
+		return processingController;
+	}
+
+	public SettingsController getSettingsController()
+	{
+		return settingsController;
+	}
+
+	public void show()
+	{
+		if (!primaryStage.isShowing())
+		{
+			showImpl();
+		}
+	}
+
+	public void hide()
+	{
+		if (primaryStage.isShowing())
+		{
+			hideImpl();
+		}
+	}
+
+	public void toggleShowHide()
+	{
+		if (primaryStage.isShowing())
+		{
+			hideImpl();
+		}
+		else
+		{
+			showImpl();
+		}
+	}
+
+	private void showImpl()
+	{
+		primaryStage.show();
+		SwingUtilities.invokeLater(() -> systemTrayShowHideMenuItem.setLabel("Hide"));
+	}
+
+	private void hideImpl()
+	{
+		primaryStage.hide();
+		SwingUtilities.invokeLater(() -> systemTrayShowHideMenuItem.setLabel("Open"));
+	}
+
+	public void reloadProcessingPane() throws IOException
+	{
+		BorderPane processingPane = FxUtil.loadFromFxml("ProcessingView.fxml", "ProcessingView", Locale.ENGLISH, processingController);
+		AnchorPane.setTopAnchor(processingPane, 0.0d);
+		AnchorPane.setRightAnchor(processingPane, 0.0d);
+		AnchorPane.setBottomAnchor(processingPane, 0.0d);
+		AnchorPane.setLeftAnchor(processingPane, 0.0d);
+		processingRootPane.getChildren().setAll(processingPane);
+	}
+
+	public void selectTab(int index)
+	{
+		tabPane.getSelectionModel().select(index);
+	}
+
 	public boolean isSystemTrayAvailable()
 	{
-		return trayIcon != null;
+		// if systemTrayIcon is not null, then systemTray and systemTrayIcon were successfully initialized
+		return systemTrayIcon != null;
 	}
 
 	public void displaySystemTrayNotification(String caption, String text, MessageType messageType)
 	{
 		if (isSystemTrayAvailable())
 		{
-			javax.swing.SwingUtilities.invokeLater(() -> trayIcon.displayMessage(caption, text, messageType));
+			javax.swing.SwingUtilities.invokeLater(() -> systemTrayIcon.displayMessage(caption, text, messageType));
 		}
+	}
+
+	public void exit()
+	{
+		primaryStage.fireEvent(new WindowEvent(primaryStage, WindowEvent.WINDOW_CLOSE_REQUEST));
 	}
 
 	@Override
@@ -263,7 +302,20 @@ public class MainController extends AbstractController
 			commonExecutor.awaitTermination(10, TimeUnit.SECONDS);
 		}
 
-		// Remove the SystemTray icon to shutdown AWT EventQueue
-		javax.swing.SwingUtilities.invokeLater(() -> systemTray.remove(trayIcon));
+		SwingUtilities.invokeLater(this::removeSystemTrayIcon);
+	}
+
+	/**
+	 * Removes the TrayIcon. Must be done on application shutdown because the SystemTrayIcon causes the AWT Event Dispatch Thread to keep running.
+	 * <p>
+	 * <b>HAS to be invoked in AWT Event Dispatch Thread.</b>
+	 * </p>
+	 */
+	private void removeSystemTrayIcon()
+	{
+		if (systemTray != null)
+		{
+			systemTray.remove(systemTrayIcon);
+		}
 	}
 }
