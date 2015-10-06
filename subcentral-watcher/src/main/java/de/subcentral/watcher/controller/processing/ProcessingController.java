46666a744287cfb1b75d16170233ebb2e62295a7
negromanus <negromanus90@gmail.com>
2015-10-06 17:23:59 +0200

    test

--- subcentral-watcher/src/main/java/de/subcentral/watcher/controller/processing/ProcessingController.java
+++ subcentral-watcher/src/main/java/de/subcentral/watcher/controller/processing/ProcessingController.java
@@ -848,6 +848,7 @@ public class ProcessingController extends AbstractController
 	public void reprocess(ProcessingTask task)
 	{
 		// Cancel the current task
+
 		task.cancel(true);
 
 		TreeItem<ProcessingItem> treeItem = task.getTaskTreeItem();
