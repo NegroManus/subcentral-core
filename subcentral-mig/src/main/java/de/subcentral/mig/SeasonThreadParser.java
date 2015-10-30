0d9f75e27805b0a754cdbecc4ac6c4ccd0093072
negromanus <negromanus90@gmail.com>
2015-10-30 15:50:05 +0100

    Worked on SeasonThreadParser;

--- subcentral-mig/src/main/java/de/subcentral/mig/SeasonThreadParser.java
+++ subcentral-mig/src/main/java/de/subcentral/mig/SeasonThreadParser.java
@@ -1,11 +1,14 @@
 package de.subcentral.mig;
 
 import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import de.subcentral.core.metadata.media.Season;
+import de.subcentral.core.metadata.media.Series;
 import de.subcentral.support.subcentralde.SubCentralApi;
 import de.subcentral.support.subcentralde.SubCentralHttpApi;
 
@@ -35,7 +38,7 @@ public class SeasonThreadParser
 	{
 		Season season = new Season();
 
-		System.out.println(postTitle);
+		parsePostTitle(season, postTitle);
 		System.out.println(postContent);
 
 		return season;
@@ -43,12 +46,51 @@ public class SeasonThreadParser
 
 	/**
 	 * <pre>
+	 * Numbered:
 	 * Mr. Robot - Staffel 1 - [DE-Subs: 10 | VO-Subs: 10] - [Komplett] - [+ Deleted Scenes]
+	 * 
+	 * Special with title:
+	 * Doctor Who - Klassische Folgen - [DE-Subs: 111 | VO-Subs: 160 | Aired: 160] - [+Specials]
+	 * Psych - Webisodes - [DE-Subs: 06 | VO-Subs: 06] - [Komplett]
+	 * 
+	 * Multiple seasons in one thread:
+	 * Buffy the Vampire Slayer - Staffel 1 bis Staffel 7 - Komplett
 	 * </pre>
 	 * 
 	 */
 	private void parsePostTitle(Season season, String postTitle)
 	{
+		Matcher numberedMatcher = Pattern.compile("(.*)\\s*-\\s*Staffel\\s+(\\d+)\\s*-\\s*.*").matcher(postTitle);
+		if (numberedMatcher.matches())
+		{
+			Series series = new Series(numberedMatcher.group(1));
+			season.setSeries(series);
+			Integer number = Integer.valueOf(numberedMatcher.group(2));
+			season.setNumber(number);
+			return;
+		}
+
+		Matcher specialMatcher = Pattern.compile("(.*)\\s*-\\s*([\\w\\s]+)\\s*-\\s*.*").matcher(postTitle);
+		if (specialMatcher.matches())
+		{
+			Series series = new Series(specialMatcher.group(1));
+			season.setSeries(series);
+			String title = specialMatcher.group(2);
+			season.setTitle(title);
+			season.setSpecial(true);
+			return;
+		}
+
+		Matcher multipleMatcher = Pattern.compile("(.*)\\s*-\\s*Staffel\\s+(\\d+)\\s+bis\\s+Staffel\\s+(\\d+)\\s*-\\s*.*").matcher(postTitle);
+		if (multipleMatcher.matches())
+		{
+			Series series = new Series(multipleMatcher.group(1));
+			season.setSeries(series);
+			String title = specialMatcher.group(2);
+			season.setTitle(title);
+			season.setSpecial(true);
+			return;
+		}
 
 	}
 
