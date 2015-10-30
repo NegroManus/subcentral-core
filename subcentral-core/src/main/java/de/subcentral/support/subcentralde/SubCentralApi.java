7d40c769459ef462de81e32d50bd3d3e40f8b0ea
negromanus <negromanus90@gmail.com>
2015-10-30 11:16:47 +0100

    SubCentral-API: documented the not working state;

--- subcentral-core/src/main/java/de/subcentral/support/subcentralde/SubCentralApi.java
+++ subcentral-core/src/main/java/de/subcentral/support/subcentralde/SubCentralApi.java
@@ -7,23 +7,21 @@ import org.jsoup.nodes.Document;
 
 public interface SubCentralApi
 {
+	/**
+	 * @deprecated Currently not working (login requires recaptcha)
+	 */
 	public void login(String username, String password) throws IOException;
 
 	/**
-	 * <pre>
-	 * wcf_userID:	21754
-	 * wcf_password:	f191d317ad977506c2e71fbbf91097f83f8bd1ca
-	 * wcf_cookieHash:	6a9df383589c676a9c4c26ba4de65059815d782c
-	 * wcf_boardLastActivityTime:	1433160906
-	 * </pre>
-	 * 
-	 * @deprecated Currently not working (gets 404 because Cookies userID and password (hash) are missing
-	 * @throws IOException
+	 * @deprecated Currently not working (gets 404 because Cookies userID and password (hash) are missing)
 	 */
 	public void logout() throws IOException;
 
 	public Document getContent(String url) throws IOException;
 
+	/**
+	 * @deprecated Currently not working (requires login which is not working)
+	 */
 	public Path downloadAttachment(int attachmentId, Path directory) throws IOException;
 
 }
