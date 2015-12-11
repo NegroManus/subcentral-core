4867b0f42610a3c2145bcd2b0c59dbe808296137
negromanus <negromanus90@gmail.com>
2015-12-11 13:31:56 +0100

    Worked on Namer;

---
+++ subcentral-core/src/main/java/de/subcentral/core/naming/NamedMediaNamer.java
@@ -0,0 +1,24 @@
+package de.subcentral.core.naming;
+
+import java.util.Map;
+
+import de.subcentral.core.metadata.media.Movie;
+import de.subcentral.core.metadata.media.NamedMedia;
+
+/**
+ * Generic Namer for NamedMedia instances.
+ */
+public class NamedMediaNamer extends AbstractNamedMediaNamer<NamedMedia>
+{
+	public NamedMediaNamer(PropSequenceNameBuilder.Config config)
+	{
+		super(config);
+	}
+
+	@Override
+	public void buildName(PropSequenceNameBuilder b, NamedMedia media, Map<String, Object> params)
+	{
+		String name = NamingUtil.readParameter(params, PARAM_NAME, String.class, media.getName());
+		b.appendIfNotNull(Movie.PROP_NAME, name);
+	}
+}
