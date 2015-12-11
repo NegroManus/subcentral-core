595568f51992aba260db00086498f2620884f890
negromanus <negromanus90@gmail.com>
2015-12-11 15:47:33 +0100

    Util: added Predicates;

---
+++ subcentral-core/src/main/java/de/subcentral/core/util/Predicates.java
@@ -0,0 +1,19 @@
+package de.subcentral.core.util;
+
+import java.util.Objects;
+import java.util.function.Predicate;
+
+public class Predicates
+{
+	public static Predicate<Object> instanceOf(Class<?> type)
+	{
+		Objects.requireNonNull(type, "type");
+		return (Object o) -> type.isInstance(o);
+	}
+
+	public static Predicate<Object> sameType(Class<?> type)
+	{
+		Objects.requireNonNull(type, "type");
+		return (Object o) -> o != null && type.equals(o.getClass());
+	}
+}
