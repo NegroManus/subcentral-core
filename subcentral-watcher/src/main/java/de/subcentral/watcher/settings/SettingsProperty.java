96aeaed52eb8d7d1f8666c569a5efced6e9a93cc
negromanus <negromanus90@gmail.com>
2016-04-04 15:25:32 +0200

    Worked on SettingsProperty interface;

--- subcentral-watcher/src/main/java/de/subcentral/watcher/settings/SettingsProperty.java
+++ subcentral-watcher/src/main/java/de/subcentral/watcher/settings/SettingsProperty.java
@@ -2,13 +2,23 @@ package de.subcentral.watcher.settings;
 
 import org.apache.commons.configuration2.XMLConfiguration;
 
+import javafx.beans.binding.BooleanBinding;
 import javafx.beans.property.Property;
+import javafx.beans.property.ReadOnlyProperty;
 
-public abstract class SettingsProperty<T>
+public interface SettingsProperty<T>
 {
-	public abstract Property<T> getProperty();
+	public Property<T> getCurrent();
 
-	public abstract void load(XMLConfiguration cfg);
+	public ReadOnlyProperty<T> getOriginal();
 
-	public abstract void save(XMLConfiguration cfg);
+	public BooleanBinding changedBinding();
+
+	public boolean hasChanged();
+
+	public void reset();
+
+	public void load(XMLConfiguration cfg);
+
+	public void save(XMLConfiguration cfg);
 }
