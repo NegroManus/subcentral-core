defd6972d01da700013b736586075ab1720672cd
negromanus <negromanus90@gmail.com>
2016-04-13 15:55:44 +0200

    minor;

--- subcentral-watcher/src/main/java/de/subcentral/fx/settings/SettingsProperty.java
+++ subcentral-watcher/src/main/java/de/subcentral/fx/settings/SettingsProperty.java
@@ -1,14 +1,11 @@
 package de.subcentral.fx.settings;
 
 import javafx.beans.property.Property;
+import javafx.beans.value.WritableValue;
 
-public interface SettingsProperty<T, P extends Property<T>> extends Settable
+public interface SettingsProperty<T, P extends Property<T>> extends Settable, WritableValue<T>
 {
 	public String getKey();
 
 	public P property();
-
-	public T getValue();
-
-	public void setValue(T value);
 }
