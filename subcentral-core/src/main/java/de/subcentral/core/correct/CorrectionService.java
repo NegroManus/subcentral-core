a6f3d562b5716d983064fb56750e40a8304e253e
NegroManus <negromanus@yahoo.de>
2016-04-28 01:20:20 +0200

    CorrectionService now ext. Function;

--- subcentral-core/src/main/java/de/subcentral/core/correct/CorrectionService.java
+++ subcentral-core/src/main/java/de/subcentral/core/correct/CorrectionService.java
@@ -1,10 +1,17 @@
 package de.subcentral.core.correct;
 
 import java.util.List;
+import java.util.function.Function;
 
 import de.subcentral.core.util.Service;
 
-public interface CorrectionService extends Service
+public interface CorrectionService extends Service, Function<Object, List<Correction>>
 {
 	public List<Correction> correct(Object bean);
+
+	@Override
+	default List<Correction> apply(Object t)
+	{
+		return correct(t);
+	}
 }
